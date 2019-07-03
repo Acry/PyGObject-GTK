fc2caaf0294982e261fddc56c7218a2f4095b988
Carsten Holtkamp <caholtk@gmail.com>
2019-07-03 14:37:52 +0200

    New Demo

---
+++ Gtk/Text/View/Drawing/03 Coordinates.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+"""
+03 Coordinates.py
+
+Some filling text.
+
+More filling text.
+"""
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository import Gio
+from gi.repository.GdkPixbuf import Pixbuf
+from gi.repository import Gdk
+import sys
+import os
+
+ICON_IMAGE = os.path.join('gtk-logo.svg')
+RED_APPLE = os.path.join('apple-red.png')
+CSS_FILE = os.path.join('view.css')
+
+TEXT = __doc__ + __doc__ + __doc__ + __doc__ + __doc__
+
+
+class MyTextView(Gtk.ApplicationWindow):
+    pb = None
+
+    def __init__(self, app):
+        Gtk.Window.__init__(self, title="TextView Example", application=app)
+        self.set_border_width(10)
+        self.set_default_size(450, 450)
+        self.set_icon_from_file(ICON_IMAGE)
+        self.pb = Pixbuf.new_from_file(RED_APPLE)
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        buf = Gtk.TextBuffer()
+        buf.set_text(TEXT)
+        textview = Gtk.TextView(buffer=buf)
+        textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        scrolled_window.add(textview)
+        textview.connect('draw', self.draw_cb)
+        # region CSS
+        style_provider = Gtk.CssProvider()
+        style_provider.load_from_path(CSS_FILE)
+        context = textview.get_style_context()
+        context.add_provider(
+            style_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+        # endregion
+        self.add(scrolled_window)
+
+    def draw_cb(self, widget, cr):
+        gdk_window = widget.get_window(1)
+        surf = Gdk.cairo_surface_create_from_pixbuf(self.pb, 1, gdk_window)
+        if surf:
+            coords = widget.buffer_to_window_coords(1, 57, 150)
+            cr.set_source_surface(surf, coords[0], coords[1])
+            cr.paint()
+            del surf
+        return False
+
+
+class TextViewExample(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self,
+                                 application_id="io.Acry.TextViewExample",
+                                 flags=Gio.ApplicationFlags.FLAGS_NONE)
+
+    def do_activate(self):
+        win = MyTextView(self)
+        win.show_all()
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+
+if __name__ == '__main__':
+    app = TextViewExample()
+    exit_status = app.run(sys.argv)
+    sys.exit(exit_status)
